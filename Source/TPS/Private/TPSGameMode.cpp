512383a837a2c2de44a0b09c69ce68017b36dde9
f11m4at4 <>
2025-02-06 14:34:11 +0900

    Initial commit

---
+++ Source/TPS/Private/TPSGameMode.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSGameMode.h"
+
+#include "TPS.h"
+
+ATPSGameMode::ATPSGameMode()
+{
+}
+
+void ATPSGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//PRINTLOGTOSCREEN(TEXT("%s"), TEXT("Hello World"));
+	// PrintLogFunc(TEXT("%s"), TEXT("Hello World"));
+}
---
+++ Source/TPS/Public/TPS.h
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#define CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+#define PRINT_CALLINFO() \
+UE_LOG(LogTemp, Warning, TEXT("%s"), *CALLINFO)
+
+#define PRINTLOG(fmt, ...) \
+UE_LOG(LogTemp, Warning, TEXT("%s %s"), *CALLINFO, *FString::Printf(fmt, ##__VA_ARGS__))
+
+#define PRINTLOGTOSCREEN(fmt, ...) \
+PRINTLOG(fmt, ##__VA_ARGS__);\
+GEngine->AddOnScreenDebugMessage(0, 2, FColor::Cyan, *FString::Printf(fmt, ##__VA_ARGS__))
+
+inline void PrintLogFunc(const TCHAR* fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	TCHAR buffer[1024];
+	FCString::GetVarArgs(buffer, UE_ARRAY_COUNT(buffer), fmt, args);
+	PRINTLOG(TEXT("%s"), buffer);
+
+	GEngine->AddOnScreenDebugMessage(0, 2, FColor::Cyan, *FString::Printf(TEXT("%s"), buffer));
+
+	va_end(args);
+}
